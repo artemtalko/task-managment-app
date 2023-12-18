f468a3098c373bbe66b8c2508c65a854daca30ae
artem.talko <artem.talko@4k.com.ua>
2023-12-18 22:41:20 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./componets/Header";
+import Center from "./componets/Center";
+
+function App() {
+  return (
+    <div>
+      {/* Header Section */}
+      <Header/>
+
+      {/* Center Section */}
+      <Center/>
+
+    </div>
+  )
+}
+
+export default App
+
