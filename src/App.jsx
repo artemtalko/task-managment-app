2fd1f4c41bd64d945871068d2e36da049883d2a3
1Klas <62036807+1Klas@users.noreply.github.com>
2023-12-23 16:02:27 +0200

    EmptyBoard component and behavior, Start of columns and tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "./componets/Header";
 import Center from "./componets/Center";
-import { useState } from "react";
+import EmptyBoard from "./componets/EmptyBoard";
+import boardsSlice from "./redux/boardsSlice";
 
 function App() {
   const [isBoardModalOpen, setIsBoardModalOpen] = useState(false);
-
+  const dispatch = useDispatch();
+  const boards = useSelector((state) => state.boards);
+  const activeBoard = boards.find((board) => board.isActive);
+  if (!activeBoard && boards.length > 0) {
+    dispatch(boardsSlice.actions.setBoardActive({ index: 0 }));
+  }
   return (
-    <div>
-      {/* Header Section */}
-      <Header
+    <div className=" overflow-hidden  overflow-x-scroll">
+      <>
+        {boards.length > 0 ?
+        <>
+        <Header
           setIsBoardModalOpen={setIsBoardModalOpen}
           isBoardModalOpen={isBoardModalOpen}
-      />
-      
-      {/* Center Section */}
-      <Center/>
-
+        />
+        <Center
+          setIsBoardModalOpen={setIsBoardModalOpen}
+          isBoardModalOpen={isBoardModalOpen}
+        />
+        </>
+        :
+        <>
+          <EmptyBoard type='add'/>
+        </>
+      }
+        
+      </>
     </div>
-  )
+  );
 }
 
 export default App
-
--- src/componets/Center.jsx
+++ src/componets/Center.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import SideBar from './SideBar'
 
-function Center() {
+function Center(isBoardModalOpen, setIsBoardModalOpen) {
+  const [windowSize, setWindowSize] = useState([window.innerWidth, window.innerHeight]);
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+
+  const boards = useSelector((state) => state.boards);
+  const activeBoard = boards.find((board) => board.isActive);
+  const columns = activeBoard.columns;
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div>Center</div>
+    <div 
+      className={
+        windowSize[0] > 768 && isSideBarOpen ? "bg-[#f4f7fd] dark:bg-[#20212c] scrollbar-hide h-screen flex overflow-x-scroll gap-6 ml-[261px]" :
+        "bg-[#f4f7fd] dark:bg-[#20212c] scrollbar-hide h-screen flex overflow-x-scroll gap-6"
+      }
+    >
+      {
+        windowSize[0] >= 768 && (
+          <SideBar/>
+        )
+      }
+
+      {/* Columns Section */}
+
+      {
+
+      }
+
+      </div>
   )
 }
 
